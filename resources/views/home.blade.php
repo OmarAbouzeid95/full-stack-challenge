e65c3ad4948f962898f20b5a0f3b5e8fe41b6839
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2025-03-02 00:09:20 -0500

    add home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,11 @@
 @extends('layout')
 
 @section('content')
-  <h1 class="text-3xl font-bold text-center text-black dark:text-purple-300">Hello, world</h1>
+  <section class="py-26 px-6 flex items-center justify-center" x-data>
+    <div>
+      <h1 class="font-bold text-5xl mx-auto text-center mb-12" x-transition:enter-start="translate-y-100">Welcome to <span class="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">WiseJobs</span></h1>
+      <p class="text-xl max-w-lg mb-8 text-text-light-secondary dark:text-text-dark-secondary text-center mb-8">Find your dream job from top companies or discover talented candidates who can help your business grow.</p>
+      <a href="/jobs" class="block mx-auto w-fit bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white rounded-full px-6 py-4 text-xl">Job Listings</a>
+    </div> 
+  </section>
 @endsection
