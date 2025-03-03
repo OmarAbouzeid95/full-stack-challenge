8e9b68e35cd0639b829b7835204e7dafc316a43e
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2025-03-02 19:22:23 -0500

    fix global theme persistence

--- resources/js/alpine/store.ts
+++ resources/js/alpine/store.ts
@@ -5,6 +5,7 @@ type Role = "user" | "admin";
 interface GlobalStore {
     theme: Theme;
     role: Role;
+    handleThemeClass: () => void;
     toggleTheme: () => void;
     makeAdmin: () => void;
     makeUser: () => void;
@@ -13,9 +14,17 @@ interface GlobalStore {
 const globalStore: GlobalStore = {
     theme: (localStorage.getItem("theme") as Theme) || "light",
     role: (localStorage.getItem("role") as Role) || "user",
+    handleThemeClass() {
+        if (localStorage.getItem("theme") === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    },
     toggleTheme() {
         this.theme = this.theme === "dark" ? "light" : "dark";
         localStorage.setItem("theme", this.theme);
+        this.handleThemeClass();
     },
     makeAdmin() {
         this.role = "admin";
