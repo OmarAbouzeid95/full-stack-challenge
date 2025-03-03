c6e93b94c8191a557235726e05b610e8dac980bb
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2025-03-02 21:07:36 -0500

    update to CompanySeeder for logos

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -9,17 +9,28 @@ use Faker\Factory as Faker;
 
 class CompanySeeder extends Seeder
 {
-    public function run()
-    {
-        $faker = Faker::create();
 
-        for ($i = 0; $i < 10; $i++) {
-            Company::create([
-                'name' => $faker->company,
-                'about' => $faker->paragraph,
-                'logo' => $faker->imageUrl(400, 400, 'business', true),
-            ]);
-        }
+    public function run()
+{
+    $faker = Faker::create();
+
+    $logos = [
+        'https://www.reshot.com/preview-assets/icons/LSZVDUKX6M/telegram-LSZVDUKX6M.svg',
+        'https://www.reshot.com/preview-assets/icons/CG78KNX46Q/behance-CG78KNX46Q.svg',
+        'https://www.reshot.com/preview-assets/icons/GE2JYXASCZ/spotify-GE2JYXASCZ.svg',
+        'https://www.reshot.com/preview-assets/icons/UKWGS8QJBT/pinterest-UKWGS8QJBT.svg'
+    ];
+
+    for ($i = 0; $i < 10; $i++) {
+        $logo = $logos[array_rand($logos)];
+
+        Company::create([
+            'name' => $faker->company,
+            'about' => $faker->paragraph,
+            'logo' => $logo,
+        ]);
     }
+}
+
 }
 
